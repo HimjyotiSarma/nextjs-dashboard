811207d99c91a9c7be16034adc6529ac79c6e624
Himjyoti <127434307+HimjyotiSarma@users.noreply.github.com>
2025-08-06 17:49:30 +0530

    Add Basic App pages

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import DashboardSkeleton from '@/app/ui/skeletons'
+
+export default function Loading() {
+  return (
+    // <div className="h-full flex items-center justify-center">
+    //   <div className="h-16 w-16 border-8 border-t-blue-600 border-r-transparent border-b-transparent border-l-transparent rounded-full animate-spin"></div>
+    // </div>
+    <DashboardSkeleton />
+  )
+}
